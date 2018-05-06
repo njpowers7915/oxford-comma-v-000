484df1e31d49f60769e9315afda2f2ecd9317728
Nicholas Powers <njpowers7915@gmail.com>
2018-05-06 04:26:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,7 @@ def oxford_comma(array)
   first_string = array.join(", ")
   new_array = first_string.split(" + ")
   oxford_comma_array = new_array[-1].split(",")
+  oxford_comma_array[-1].strip
   new_array[-1] = oxford_comma_array.join(" ")
   new_array.join(" ")
 end
