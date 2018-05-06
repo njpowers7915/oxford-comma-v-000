0ba082fa977607bc8ffaf15591ff968ca9bf8e1d
Nicholas Powers <njpowers7915@gmail.com>
2018-05-06 04:23:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,7 @@ def oxford_comma(array)
   array.insert(-2, "+ and")
   first_string = array.join(", ")
   new_array = first_string.split(" + ")
-  oxford_comma_element = new_array[-1].split(",")
+  oxford_comma_array = new_array[-1].split(",")
+  new_array[-1] = oxford_comma_array.join(" ")
+  new_array.join(" ")
 end
