424d33f687dc5f6097297bb5c9a9ba6ab68b3c3b
Nicholas Powers <njpowers7915@gmail.com>
2018-05-06 04:37:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.size >= 3
+    array.insert(-2, "+ and")
+    first_string = array.join(", ")
+    new_array = first_string.split(" + ")
+    oxford_comma_array = new_array[-1].split(",")
+    new_array[-1] = oxford_comma_array.join
+    new_array.join(" ")
+  elsif array.size == 2
+    array.insert(1, "and")
+    array.join(" ")
+  elsif array.size == 1
+    array.join
+  end
 end
