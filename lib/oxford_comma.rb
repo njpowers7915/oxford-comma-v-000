fe447279f79daa016cc9720668a57473a864a6d5
Nicholas Powers <njpowers7915@gmail.com>
2018-05-06 04:32:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,7 +8,7 @@ def oxford_comma(array)
     new_array[-1] = oxford_comma_array.join("")
     new_array.join(" ")
   elsif array.size == 2
-    array.insert(1, and)
+    array.insert(1, "and")
     array.join(" ")
   elsif array.size == 1
     array.join
