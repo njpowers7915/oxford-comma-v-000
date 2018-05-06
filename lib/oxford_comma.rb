60c410a68fd17ba7118149f1a27cb362fe0b4af5
Nicholas Powers <njpowers7915@gmail.com>
2018-05-06 04:21:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,6 @@
 def oxford_comma(array)
-  array.insert(-2, "and")
+  array.insert(-2, "+ and")
+  first_string = array.join(", ")
+  new_array = first_string.split(" + ")
+  oxford_comma_element = new_array[-1].split(",")
 end
